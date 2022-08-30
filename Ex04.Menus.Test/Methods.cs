cec042aea0c1884570f2178fe35f8d204b09dfb0
Yarin Bar <yarin0008@gmail.com>
2022-08-30 15:12:28 +0300

    Minor changesss

--- Ex04.Menus.Test/Methods.cs
+++ Ex04.Menus.Test/Methods.cs
@@ -2,11 +2,11 @@
 
 namespace Ex04.Menus.Test
 {
-    public class Methods
+    internal class Methods
     {
         private const string k_CurrentAppVersion = "Version: 22.3.4.8650";
 
-        public static void CountSpacesOfSentence_LaunchedMethod()
+        internal static void CountSpacesOfSentence_LaunchedMethod()
         {
             int numOfSpacesInSentence = 0;
             Console.WriteLine("Please type your sentence:");
@@ -24,19 +24,19 @@ namespace Ex04.Menus.Test
             Console.WriteLine(amountOfSpacesMessage);
         }
 
-        public static void ShowVersion_LaunchedMethod()
+        internal static void ShowVersion_LaunchedMethod()
         {
             Console.WriteLine(k_CurrentAppVersion);
         }
 
-        public static void ShowDate_LaunchedMethod()
+        internal static void ShowDate_LaunchedMethod()
         {
             DateTime currentDate = DateTime.UtcNow;
             string currentDateMessage = $"The current date is {currentDate.Date:d}.";
             Console.WriteLine(currentDateMessage);
         }
 
-        public static void ShowTime_LaunchedMethod()
+        internal static void ShowTime_LaunchedMethod()
         {
             DateTime currentTime = DateTime.Now;
             string currentTimeMessage = $"The current time is {currentTime:HH:mm:ss tt}.";
